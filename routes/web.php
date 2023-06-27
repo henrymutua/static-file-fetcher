169f35b75c451fde33c1fac79e6257ffb7fd8f89
henrymutua <mutuahenry116@gmail.com>
2023-06-27 16:30:48 +0300

    Added homepage

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use App\Http\Controllers\FileController;
 
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::get('/files/{type}/{filename}', [FileController::class, 'fetch'])->name('fetch.file');
