51272c07efd85920b7526b19341e45d14955b3a3
henrymutua <mutuahenry116@gmail.com>
2023-06-27 15:53:58 +0300

    Changed the file path

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,19 +1,33 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
-    public function fetch($filename)
+    public function fetch($type, $filename)
     {
-        $path = storage_path('app/public/' . $filename);
+        $folder = $this->getFolder($type);
+        $path = storage_path("app/public/files/{$folder}/{$filename}");
 
-        if (Storage::exists($filename)) {
+        if (Storage::exists("public/files/{$folder}/{$filename}")) {
             return response()->file($path);
         } else {
             abort(404);
         }
+    }
+
+    private function getFolder($type)
+    {
+        $folders = [
+            'images' => 'images',
+            'videos' => 'videos',
+            'docs' => 'docs',
+            'audio' => 'audio',
+        ];
+
+        return $folders[$type] ?? '';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use App\Http\Controllers\FileController;
 
-Route::get('/files/{filename}', [FileController::class, 'fetch']);
+Route::get('/files/{type}/{filename}', [FileController::class, 'fetch'])->name('fetch.file');
